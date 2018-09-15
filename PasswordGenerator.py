9413c94d50f0dbb8d5ce22272ae45a9b50034aa3
William Griffin <40611326+evelandy@users.noreply.github.com>
2018-09-15 01:03:39 -0500

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,7 +1,7 @@
 import random
 
-length = int(input("Please enter a length for your password: "))
-amount = int(input("How many passwords would you like to choose from? "))
+length = int(input("Please enter a length for your password: ")) # Takes user input to create the length in characters for the password
+amount = int(input("How many passwords would you like to choose from? ")) # This input will create more than one password at a time to choose from
 characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890?.!@#$&*%$-'
 password = ''
 
